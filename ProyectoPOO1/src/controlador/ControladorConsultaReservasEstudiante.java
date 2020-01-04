011d32b03bd826a8e5695127985aee76d22f0da4
andres2018151725 <andres05022000@gmail.com>
2020-01-04 15:01:19 -0800

    Merge origin/master
    Conflicts:
    	ProyectoPOO1/src/dao/ConexionSQL.java

---
+++ ProyectoPOO1/src/controlador/ControladorConsultaReservasEstudiante.java
@@ -0,0 +1,28 @@
+package controlador;
+
+
+import dao.ReservaDAO;
+import gestionador.ControladorGestionador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import modelo.Estudiante;
+import modelo.Reserva;
+import modelo.Incidente;
+import vista.ConsultaEstudiantesForm;
+import vista.Menu;
+/**
+ *
+ * @author Andrés Pérez Bonilla
+ */
+public class ControladorConsultaReservasEstudiante {
+    
+}
