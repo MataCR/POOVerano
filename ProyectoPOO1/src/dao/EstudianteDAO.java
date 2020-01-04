652c20758d2947944360c324cf240aec5ddfddc6
andres2018151725 <andres05022000@gmail.com>
2020-01-04 15:02:01 -0800

    Merge origin/master
    Conflicts:
    	ProyectoPOO1/src/dao/ConexionSQL.java

--- ProyectoPOO1/src/dao/EstudianteDAO.java
+++ ProyectoPOO1/src/dao/EstudianteDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import modelo.Estudiante;
 import modelo.Reserva;
+import modelo.Incidente;
 
 
 /**
@@ -19,7 +20,8 @@ public class EstudianteDAO {
   public boolean agregarOrganizador(Estudiante organizador) throws SQLException,
       ClassNotFoundException {
     try {
-      CallableStatement entrada = ConexionSQL.getConexionSQL().prepareCall("{call agregarOrganizador (?,?,?,?,?,?,?,?)}");
+      CallableStatement entrada = ConexionSQL.getConexionSQL().prepareCall("{call "
+          + "agregarOrganizador (?,?,?,?,?,?,?,?)}");
       entrada.setInt(1, organizador.getCarnet());
       entrada.setString(2, organizador.getNombre());
       entrada.setString(3, organizador.getPrimerApellido());
@@ -47,38 +49,53 @@ public class EstudianteDAO {
     return carreras;
   }
   
-  
-  /*
+ 
   public ArrayList<Estudiante> consultarEstudiantes(int carnet)throws SQLException,
       ClassNotFoundException{
-    resultadoConsulta = ConexionSQL.createConsult("exec consultarOrganizadorIncidente "
+    resultadoConsulta = ConexionSQL.createConsult("exec consultarOrganizador "
         +carnet+";");
     ArrayList<Estudiante> estudiantes = new ArrayList<Estudiante>();
-    while (resultadoConsulta.next()) {
-       
+    while (resultadoConsulta.next()) {    
       Estudiante estudianteEncontrado = new Estudiante(resultadoConsulta.getInt(1),
           resultadoConsulta.getString(2), resultadoConsulta.getString(3),
           resultadoConsulta.getString(4), resultadoConsulta.getString(5),
-          resultadoConsulta.getString(6), resultadoConsulta.getInt(7));
+          resultadoConsulta.getInt(6), resultadoConsulta.getInt(7),resultadoConsulta.getString(8));
       estudiantes.add(estudianteEncontrado);
     }
     return estudiantes;
-  }
+  }          
+    
   
-   
-  private ArrayList<Reserva> consultarReservasEstudiante(int carnet)throws SQLException,
+  public ArrayList<Reserva> consultarReservasEstudiante(int carnet)throws SQLException,
       ClassNotFoundException{
     resultadoConsulta = ConexionSQL.createConsult("exec consultarOrganizadorReserva "+carnet+";");
     ArrayList<Reserva> reservasEstudiante = new ArrayList<Reserva>();
     while (resultadoConsulta.next()) {
-      Estudiante estudianteEncontrado 
-              = new Estudiante(resultadoConsulta.getInt(1),
-          resultadoConsulta.getString(2), resultadoConsulta.getString(3),
-          resultadoConsulta.getString(4), resultadoConsulta.getString(5),
-          resultadoConsulta.getString(6), resultadoConsulta.getInt(7));
-      estudiantes.add(estudianteEncontrado);
+      boolean isExitosa = false;
+      if (resultadoConsulta.getInt(9) == 1){
+        isExitosa = true;
+      }
+      Reserva reservaEncontrada = new Reserva(resultadoConsulta.getInt(1),
+          resultadoConsulta.getString(2), resultadoConsulta.getDate(3),
+          resultadoConsulta.getDate(4), resultadoConsulta.getTime(5),
+          resultadoConsulta.getTime(6), resultadoConsulta.getString(7),
+          resultadoConsulta.getString(8), isExitosa);
+      reservasEstudiante.add(reservaEncontrada);
     }
-    return estudiantes;
+    return reservasEstudiante;
   }
-*/
+  
+  
+  public ArrayList<Incidente> consultarReservasIncidentesEstudiante(int carnet)throws 
+      SQLException,ClassNotFoundException{
+    resultadoConsulta = ConexionSQL.createConsult("exec consultarOrganizadorIncidente "
+        +carnet+";");
+    ArrayList<Incidente> incidentesEstudiante = new ArrayList<Incidente>();
+    while (resultadoConsulta.next()) {
+      Incidente incidenteEncontrado = new Incidente(resultadoConsulta.getInt(1), 
+          resultadoConsulta.getString(2),resultadoConsulta.getInt(3));
+      incidentesEstudiante.add(incidenteEncontrado);
+    }
+    return incidentesEstudiante;
+  }
 }
