db49e90b7d01587996e4ede5be98976c418d0845
andres2018151725 <andres05022000@gmail.com>
2020-01-04 16:35:24 -0800

    Merge origin/master
    Conflicts:
    	ProyectoPOO1/src/dao/ConexionSQL.java

--- ProyectoPOO1/src/dao/ReservaDAO.java
+++ ProyectoPOO1/src/dao/ReservaDAO.java
@@ -22,27 +22,12 @@ public class ReservaDAO {
     ArrayList<Reserva> reservas = new ArrayList<Reserva>();
     while (resultadoConsulta.next()) {
       Reserva reservaEncontrada = new Reserva(resultadoConsulta.getInt(1),
-          resultadoConsulta.getString(2), resultadoConsulta.getDate(3),
-          resultadoConsulta.getDate4), resultadoConsulta.getTime(5),
-          resultadoConsulta.getTime(6), resultadoConsulta.getString(7); 
-          resultadoConsulta.getString(8); resultadoConsulta.getString(9));
+        resultadoConsulta.getString(2), resultadoConsulta.getDate(3),
+        resultadoConsulta.getDate(4), resultadoConsulta.getTime(5),
+        resultadoConsulta.getTime(6), resultadoConsulta.getString(7), 
+        resultadoConsulta.getString(8), resultadoConsulta.getBoolean(9));
       reservas.add(reservaEncontrada);
     }
     return reservas;
   }
-
-  
-    this.id = pId;
-    this.asunto = pAsunto;
-    this.fechaSolicitud = pFechaSolicitud;
-    this.fechaUso = pFechaUso;
-    this.horaInicio = pHoraInicio;
-    this.horaFin = pHoraFin;
-    this.idSala = pIdSala;
-    this.estado = pEstado;
-    this.isExitosa = pIsExitosa;
-  
-  
-  
-  
 }
